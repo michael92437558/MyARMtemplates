8e355e2974892e5c3c080db9a26d8039e18ef435
eissi <julee@microsoft.com>
2016-12-18 01:16:01 +0900

    Update install.ps1

--- MyARMtemplates/VMSS/install.ps1
+++ MyARMtemplates/VMSS/install.ps1
@@ -1,14 +1,13 @@
-#
 # Script.ps1
 #
 param($url,$key)
 
 
 
-New-Item -ItemType Directory -Force -Path C:\CustomPrograms
+New-Item -ItemType Directory -Force -Path 'C:\CustomPrograms'
 Invoke-WebRequest -Uri $url -OutFile 'c:\customprograms\program.zip'
 
-Expand-Archive -Path 'c:\customprograms\program.zip' -DestinationPath ¡°c:\customprograms¡±
+Expand-Archive -Path 'c:\customprograms\program.zip' -DestinationPath 'c:\customprograms'
 
 Write-Output "URL: $url" |Out-File "c:\customprograms\test.txt"
 Write-Output "key: $key"|Out-File "c:\customprograms\test.txt" -Append
