37bffab04ad7d6b387dad3a30c464e6a191f3de1
julee <julee@KRJULEE11>
2016-12-20 14:44:04 +0900

    install script support Windwos 2012r2 and 2016

--- MyARMtemplates/VMSS/install.ps1
+++ MyARMtemplates/VMSS/install.ps1
@@ -7,7 +7,17 @@ param($url,$key)
 New-Item -ItemType Directory -Force -Path 'C:\CustomPrograms'
 Invoke-WebRequest -Uri $url -OutFile 'c:\customprograms\program.zip'
 
-Expand-Archive -Path 'c:\customprograms\program.zip' -DestinationPath 'c:\customprograms'
+if($PSVersionTable.PSVersion.Major -gt 4){
+	Expand-Archive -Path 'c:\customprograms\program.zip' -DestinationPath 'c:\customprograms'
+}
+else{
+	$shell = new-object -com shell.application
+	$zip = $shell.NameSpace(¡°c:\customprograms\program.zip¡±)
+	foreach($item in $zip.items())
+	{
+		$shell.Namespace(¡°c:\customprograms¡±).copyhere($item)
+	}
+}
 
-Write-Output "URL: $url" |Out-File "c:\customprograms\test.txt"
-Write-Output "key: $key"|Out-File "c:\customprograms\test.txt" -Append
+Write-Output "URL: $url" |Out-File "c:\customprograms\arguments.txt"
+Write-Output "key: $key" |Out-File "c:\customprograms\arguments.txt" -Append
